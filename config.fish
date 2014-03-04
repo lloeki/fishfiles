4a0e64f534609473c6e165c2319fdd7bdda32e43
Loic Nageleisen <loic.nageleisen@gmail.com>
2014-03-04 18:26:49 +0100

    homebrew

--- config.fish
+++ config.fish
@@ -3,6 +3,9 @@ set fish_greeting ""
 # rbenv
 set PATH $HOME/.rbenv/shims $PATH
 
+# brew
+set PATH /usr/local/{bin,sbin} $PATH
+
 set -g GIT_PS1_SHOWDIRTYSTATE 1
 set -g GIT_PS1_SHOWSTASHSTATE 1
 set -g GIT_PS1_SHOWUNTRACKEDFILES 1
