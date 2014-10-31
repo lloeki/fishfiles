077ea00a2c9d9e57c3eecb14650aa0ebe33cee05
Loic Nageleisen <loic.nageleisen@gmail.com>
2014-10-31 10:45:29 +0100

    chruby

--- config.fish
+++ config.fish
@@ -7,4 +7,9 @@ set PATH $HOME/.rbenv/shims $PATH
 set -x HOMEBREW_NO_EMOJI "you bet"
 set PATH /usr/local/{bin,sbin} $PATH
 
+if [ -f /usr/local/share/chruby/chruby.fish ]
+    source /usr/local/share/chruby/chruby.fish
+    chruby ruby-2.1.4
+end
+
 source ~/.config/fish/bundler.fish
