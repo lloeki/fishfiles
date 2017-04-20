240bb3e52e100541a0254afe269f7cf6f83baaa6
Loic Nageleisen <loic.nageleisen@gmail.com>
2017-04-20 16:38:06 +0200

    arch

--- config.fish
+++ config.fish
@@ -1,11 +1,11 @@
 set fish_greeting ""
 
-# rbenv
-set PATH $HOME/.rbenv/shims $PATH
-
 # brew
 set -x HOMEBREW_NO_EMOJI "you bet"
 set PATH /usr/local/{bin,sbin} $PATH
+
+# arch
+set PATH /opt/arch/{bin,sbin} $PATH
 
 if [ -f /usr/local/share/chruby/chruby.fish ]
     source /usr/local/share/chruby/chruby.fish
