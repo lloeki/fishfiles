ec7fcd70e87321384b60fca6b68a7baefb3a37a4
Loic Nageleisen <loic.nageleisen@gmail.com>
2017-04-21 11:32:48 +0200

    zap tabs

--- functions/__git_info_gitdir.fish
+++ functions/__git_info_gitdir.fish
@@ -6,30 +6,30 @@
 # Adapted from the official git bash prompt
 # TODO: Since git ~2.x and submodules, .git can be a file
 function __gitdir --description "get a folder's git dir (defaults to current)"
-	if [ -z "$argv" ]
-		if [ -n "$__git_dir" ]
-			echo "$__git_dir"
-		else if [ -d .git ]
-			echo .git
-		else
-			git rev-parse --git-dir 2>/dev/null
-		end
-	else if [ -d "$argv[1]/.git" ]
-		echo "$argv[1]/.git"
-	else
-		echo "$argv[1]"
-	end
+    if [ -z "$argv" ]
+        if [ -n "$__git_dir" ]
+            echo "$__git_dir"
+        else if [ -d .git ]
+            echo .git
+        else
+            git rev-parse --git-dir 2>/dev/null
+        end
+    else if [ -d "$argv[1]/.git" ]
+        echo "$argv[1]/.git"
+    else
+        echo "$argv[1]"
+    end
 end
 
 # Eval current dir's gitdir and cache it in an env var for performance on reuse
 # Best used only when pwd changes, to minimize stat calls
 function __git_info_gitdir --description "set GIT_INFO_GITDIR to gitdir path if available, otherwise reset it"
-	set -l g (__gitdir)
+    set -l g (__gitdir)
 
-	# are we in a git repo?
-	if [ -z "$g" ]
-		set -e GIT_INFO_GITDIR
-	else
-		set -g GIT_INFO_GITDIR "$g"
-	end
+    # are we in a git repo?
+    if [ -z "$g" ]
+        set -e GIT_INFO_GITDIR
+    else
+        set -g GIT_INFO_GITDIR "$g"
+    end
 end
